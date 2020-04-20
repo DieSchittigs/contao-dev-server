554c955cdf6cb961d87e1ff18de17ae03008dc8f
Tobias Dühr <tobias@dieschittigs.de>
2020-04-20 17:34:36 +0200

    handle case where there is no app_dev

--- src/router.php
+++ src/router.php
@@ -1,6 +1,6 @@
 <?php
 
+if (!is_file("$_SERVER[DOCUMENT_ROOT]/$script")) $script = "app.php";
 $_SERVER['SCRIPT_NAME'] = "/$script";
 $_SERVER['SCRIPT_FILENAME'] = "$_SERVER[DOCUMENT_ROOT]/$script";
-
 require_once("$_SERVER[DOCUMENT_ROOT]/$script");
